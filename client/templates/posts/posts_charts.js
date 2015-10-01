b324c8f006240fb544667a48e29b1177ae90b20f
andresjordanze <andresjordanze@gmail.com>
2015-10-01 08:16:25 -0400

    discrete bar chart

--- client/templates/posts/posts_charts.js
+++ client/templates/posts/posts_charts.js
@@ -4,17 +4,19 @@
   }
 
   Template.postsCharts.onRendered(function() {
-    var chart = nv.models.lineChart()
-      //.margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
+    /*var chart = nv.models.lineChart()
       .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
       .transitionDuration(350)  //how fast do you want the lines to transition?
       .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
       .showYAxis(true)        //Show the y-axis
-      .showXAxis(true)        //Show the x-axis
-      //.x(function(d) { return d.label })
-      //.y(function(d) { return d.label })
-      //.staggerLabels(true)
-      //.tooltips(false)
+      .showXAxis(true)        //Show the x-axis*/
+    var chart = nv.models.discreteBarChart() 
+      .x(function(d) { return d.x })
+      .y(function(d) { return d.y })
+      .staggerLabels(true)
+      .tooltips(false)
+      .showValues(true)
+      .transitionDuration(350)
     ;
 
     nv.addGraph(function() {
@@ -33,6 +35,7 @@
       ).call(chart);
       chart.update();
     });
+
   });
 
   Template.postsCharts.events({
